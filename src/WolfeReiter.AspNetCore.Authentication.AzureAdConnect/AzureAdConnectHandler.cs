a0df38214275b8e1b466a050f03e66d8081378ad
Brian Reiter <breiter@wolfereiter.com>
2018-07-24 12:09:52 +0200

    Guard against null group cache object.

--- src/WolfeReiter.AspNetCore.Authentication.AzureAdConnect/AzureAdConnectHandler.cs
+++ src/WolfeReiter.AspNetCore.Authentication.AzureAdConnect/AzureAdConnectHandler.cs
@@ -19,6 +19,7 @@ namespace WolfeReiter.AspNetCore.Authentication.AzureAD
     /// </summary>
     public class AzureAdConnectHandler : OpenIdConnectHandler, IAuthenticationSignOutHandler
     {
+        object _lock = new Object();
         new ILogger Logger { get; set; }
         ILoggerFactory LoggerFactory { get; set; }
         public AzureAdConnectHandler(IOptionsMonitor<OpenIdConnectOptions> options, ILoggerFactory logger, HtmlEncoder htmlEncoder, UrlEncoder encoder, ISystemClock clock)
@@ -64,23 +65,26 @@ namespace WolfeReiter.AspNetCore.Authentication.AzureAD
                 {
                     if (PrincipalRoleCache.TryGetValue(identityKey, out grouple))
                     {
-                        lock(grouple) //prevent concurrent access to grouple Tuple
+                        lock(_lock) //prevent concurrent access to grouple Tuple
                         {
-                            var expiration = grouple.Item1.AddSeconds(AzureAdConnectOptions.GroupCacheTtlSeconds);
-                            if (DateTime.UtcNow > expiration ||
-                                grouple.Item2.Count() != identity.Claims.Count(x => x.Type == "groups"))
+                            if(grouple != null)
                             {
-                                oldGroups = grouple.Item2;
-                                //don't need to check return because if it failed, then the entry was removed already
-                                //or we'll try again.
-                                //don't re-use the "grouple" variable because the out of TryRemove can be null.
-                                Tuple<DateTime, IEnumerable<string>> removedGrouple = null;
-                                PrincipalRoleCache.TryRemove(identityKey, out removedGrouple);
-                            }
-                            else
-                            {
-                                cacheValid = true;
-                                groups = grouple.Item2;
+                                var expiration = grouple.Item1.AddSeconds(AzureAdConnectOptions.GroupCacheTtlSeconds);
+                                if (DateTime.UtcNow > expiration ||
+                                    grouple.Item2.Count() != identity.Claims.Count(x => x.Type == "groups"))
+                                {
+                                    oldGroups = grouple.Item2;
+                                    //don't need to check return because if it failed, then the entry was removed already
+                                    //or we'll try again.
+                                    //don't re-use the "grouple" variable because the out of TryRemove can be null.
+                                    Tuple<DateTime, IEnumerable<string>> removedGrouple = null;
+                                    PrincipalRoleCache.TryRemove(identityKey, out removedGrouple);
+                                }
+                                else
+                                {
+                                    cacheValid = true;
+                                    groups = grouple.Item2;
+                                }
                             }
                         }
                     }
